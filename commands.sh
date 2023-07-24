fe28499aa75cd991c750c72796ba6e868157b826
xbit0412 <49124002+xbit0412@users.noreply.github.com>
2023-07-24 09:52:25 +0200

    Add files via upload

--- commands.sh
+++ commands.sh
@@ -1,41 +1,42 @@
 #!/bin/bash
 flatpak update --noninteractive --assumeyes
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-flatpak install flathub com.anydesk.Anydesk
-flatpak install flathub org.qbittorrent.qBittorrent
-flatpak install flathub net.davidotek.pupgui2
-flatpak install flathub com.github.Matoking.protontricks
-flatpak install flathub com.heroicgameslauncher.hgl
-flatpak install flathub org.wireshark.Wireshark
-flatpak install flathub net.lutris.Lutris
-flatpak install flathub com.usebottles.bottles
-flatpak install flathub org.audacityteam.Audacity
-flatpak install flathub org.mozilla.Thunderbird
-flatpak install flathub com.ulduzsoft.Birdtray
-flatpak install flathub org.onlyoffice.desktopeditors
-flatpak install flathub org.libreoffice.LibreOffice
-flatpak install flathub org.videolan.VLC
-flatpak install flathub org.kde.haruna
-flatpak install flathub com.microsoft.Edge
-flatpak install flathub com.obsproject.Studio
-flatpak install flathub org.gimp.GIMP
-flatpak install flathub io.github.hakandundar34coding.system-monitoring-center
-flatpak install flathub com.leinardi.gst
-flatpak install flathub com.ulduzsoft.Birdtray
-flatpak install flathub com.brave.Browser
-flatpak install flathub com.steamgriddb.SGDBoop
-flatpak install flathub com.visualstudio.code
-flatpak install flathub com.bitwarden.desktop
-flatpak install flathub org.blender.Blender
-flatpak install flathub com.discordapp.Discord
-flatpak install flathub org.duckstation.DuckStation
-flatpak install flathub net.pcsx2.PCSX2
-flatpak install flathub org.mozilla.firefox
-flatpak install flathub com.github.tchx84.Flatseal
-flatpak install flathub com.google.Chrome
-flatpak install flathub org.libretro.RetroArch
-flatpak install flathub net.rpcs3.RPCS3
-flatpak install flathub com.spotify.Client
-flatpak install flathub org.telegram.desktop
-flatpak install flathub org.gnome.Weather
+flatpak install flathub com.anydesk.Anydesk --assumeyes
+flatpak install flathub org.qbittorrent.qBittorrent --assumeyes
+flatpak install flathub net.davidotek.pupgui2 --assumeyes
+flatpak install flathub com.github.Matoking.protontricks --assumeyes
+flatpak install flathub com.heroicgameslauncher.hgl --assumeyes
+flatpak install flathub org.wireshark.Wireshark --assumeyes
+flatpak install flathub net.lutris.Lutris --assumeyes
+flatpak install flathub com.usebottles.bottles --assumeyes
+flatpak install flathub org.audacityteam.Audacity --assumeyes
+flatpak install flathub org.mozilla.Thunderbird --assumeyes
+flatpak install flathub com.ulduzsoft.Birdtray --assumeyes
+flatpak install flathub org.onlyoffice.desktopeditors --assumeyes
+flatpak install flathub org.libreoffice.LibreOffice --assumeyes
+flatpak install flathub org.videolan.VLC --assumeyes
+flatpak install flathub org.kde.haruna --assumeyes
+flatpak install flathub com.microsoft.Edge --assumeyes
+flatpak install flathub com.obsproject.Studio --assumeyes
+flatpak install flathub org.gimp.GIMP --assumeyes
+flatpak install flathub io.github.hakandundar34coding.system-monitoring-center --assumeyes
+flatpak install flathub com.leinardi.gst --assumeyes
+flatpak install flathub com.ulduzsoft.Birdtray --assumeyes
+flatpak install flathub com.brave.Browser --assumeyes
+flatpak install flathub com.steamgriddb.SGDBoop --assumeyes
+flatpak install flathub com.visualstudio.code --assumeyes
+flatpak install flathub com.bitwarden.desktop --assumeyes
+flatpak install flathub org.blender.Blender --assumeyes
+flatpak install flathub com.discordapp.Discord --assumeyes
+flatpak install flathub org.duckstation.DuckStation --assumeyes
+flatpak install flathub net.pcsx2.PCSX2 --assumeyes
+flatpak install flathub org.mozilla.firefox --assumeyes
+flatpak install flathub com.github.tchx84.Flatseal --assumeyes
+flatpak install flathub com.google.Chrome --assumeyes
+flatpak install flathub org.libretro.RetroArch --assumeyes
+flatpak install flathub net.rpcs3.RPCS3 --assumeyes
+flatpak install flathub com.spotify.Client --assumeyes
+flatpak install flathub org.telegram.desktop --assumeyes
+flatpak install flathub org.gnome.Weather --assumeyes
 echo "Flatpaks installed and updated"
+notify-send "Flatpaks installed and updated"

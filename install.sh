7fc5a728179e5f11baff9831d63a1aa72b0019dd
xbit0412 <49124002+xbit0412@users.noreply.github.com>
2023-07-24 09:14:34 +0200

    Installer script to execute core commands

---
+++ install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+wget https://github.com/xbit0412/steamdeck_automateflatpaks/blob/main/commands.sh -P $HOME/.config/autostart-scripts
+chmod +x $HOME/.config/autostart-scripts/commands.sh

67e23d4598736c0de5aec0d3f1e2640f5ee5a167
xbit0412 <49124002+xbit0412@users.noreply.github.com>
2023-07-24 09:36:06 +0200

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget https://github.com/xbit0412/steamdeck_automateflatpaks/releases/download/steamdeck/install.sh -P $HOME/.config/autostart-scripts
+wget https://github.com/xbit0412/steamdeck_automateflatpaks/releases/download/steamdeck/commands.sh -P $HOME/.config/autostart-scripts
 chmod +x $HOME/.config/autostart-scripts/commands.sh

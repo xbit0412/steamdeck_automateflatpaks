de51ec6ba17b28a15057bfa7122c991033aaeb14
xbit0412 <49124002+xbit0412@users.noreply.github.com>
2025-03-01 00:14:27 +0100

    Delete install.sh

--- install.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-wget https://github.com/xbit0412/steamdeck_automateflatpaks/releases/download/steamdeck/commands.sh -P $HOME/.config/autostart-scripts && chmod +x $HOME/.config/autostart-scripts/commands.sh
